ebf52dbac350d0bbaafaf7fdd5c46fe2d4416366
TheSunSide <sunnee17@me.com>
2020-09-10 15:55:33 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,7 +4,7 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        ord(lettre)
+        if(ord(lettre) >= 97 
         resultat += lettre
     return mot
 
