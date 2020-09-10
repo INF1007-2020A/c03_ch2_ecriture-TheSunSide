dab775580a376886aaca6e8f5ebe25a86ce2475e
TheSunSide <sunnee17@me.com>
2020-09-10 15:57:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -7,7 +7,7 @@ def majuscule(mot):
         nombre = ord(lettre)
         if(ord(nombre) >= 97 & ord(nombre) <= 122)
         {
-            resultat = chr(nombre - 32)
+            resultat = chr(nombre)
         }
         resultat += lettre
     return mot
