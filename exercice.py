ccdf61017bf1e95282e02f932541762f2f99055a
TheSunSide <sunnee17@me.com>
2020-09-10 15:57:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@ def majuscule(mot):
     for lettre in mot:
         # TODO completer la fonction ici
         nombre = ord(lettre)
-        if(ord(lettre) >= 97 & ord(lettre) <= 122)
+        if(ord(nombre) >= 97 & ord(nombre) <= 122)
         {
             resultat = chr(lettre)
         }
